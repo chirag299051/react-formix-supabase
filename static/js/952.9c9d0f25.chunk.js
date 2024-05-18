3565c0bf7915ef37b59c59460a06f6c90704d333
Chirag Singh <chirag@Chirags-MacBook-Pro.local>
2024-05-18 18:01:30 +0530

    Updates

---
+++ static/js/952.9c9d0f25.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_formix_supabase=self.webpackChunkreact_formix_supabase||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.9c9d0f25.chunk.js.map
